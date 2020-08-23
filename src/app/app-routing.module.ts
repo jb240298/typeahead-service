e669779c9359029178d0ab8e0faebb92929f7802
Krinal Patel <jaypatel24298@gmail.com>
2020-08-23 18:38:40 +0530

    added base code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TypeaheadComponent } from './typeahead/typeahead.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'search/:word',
+    component: TypeaheadComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  declarations: [],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpRequestService } from './http-request.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typeahead-front-end-service';
+  word: string;
+  responseWords: string[] = [];
+
+  constructor(private httpRequestService: HttpRequestService) { }
+
+  removeSpecialCharacters() {
+    this.word = this.word.replace(/[^a-zA-Z]/g, '');
+  }
+
+  onKey(event: any) {
+    this.word = event.target.value;
+    this.removeSpecialCharacters();
+    this.word = this.word.toLowerCase();
+    this.responseWords = [];
+    // Routing to API call.
+    this.httpRequestService.getSearch(this.word).subscribe((data: string[]) => {
+      console.log(data);
+      this.responseWords = data;
+    });
+  }
+
 }
